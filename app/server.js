be2119784494ae200daa1bef7f34a496e2280499
Ben Spoon <ben@benspoon.com>
2015-06-02 14:23:05 -0400

    Launch drone via express server

--- app/server.js
+++ app/server.js
@@ -1,5 +1,5 @@
-var express    = require('express');        
-var app        = express();                 
+var express    	  = require('express'),
+	app        	  = express();                 
 
 var port = process.env.PORT || 8080;
 
@@ -13,8 +13,33 @@ router.get('/', function(req, res) {
 
 router.route('/alert')
     .post(function(req, res) {
+    	var RollingSpider = require("rolling-spider"),
+    		temporal 	  = require("temporal"),
+    		drone = new RollingSpider();
+    	drone.connect(function() {
+    	  drone.setup(function() {
+    	    temporal.queue([
+    	    	{
+	    	        delay: 0,
+	    	        task: function () {
+	    	          drone.flatTrim();
+	    	          drone.startPing();
+	    	          console.log('=====================');
+	    	          console.log('Launching scout drone');
+	    	          console.log('=====================');
+	    	          drone.takeOff();
+    	       		}
+    	       	},
+    	       	{
+    	       	 	delay: 3000,
+    	       	 	task: function() {
+    	       	 		drone.land();
+    	       	}
+    	      }]);
+    	  });
+    	});
     	// alert that the zombies are here!
-    	res.json({ message: 'INCOMING ZOMBIES' });
+    	res.json({ message: 'Notice received' });
     });   
 
 app.use('/api', router);
