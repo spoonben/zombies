8b72ca1b641c8aaaabb71aad7ad271d9b12a109b
Ben Spoon <ben@benspoon.com>
2015-06-11 18:24:40 -0400

    Remove grunt-execute

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,8 @@
 module.exports = function(grunt) {
 
   grunt.initConfig({
-      execute: {
-          drone: {
-              src: ['scripts/drone.js']
-          },
-          server: {
-              src: ['scripts/server.js']
-          },
-          button: {
-              src: ['scripts/button.js']
-          }
-      }
+      
   });
-  grunt.loadNpmTasks('grunt-execute');
-
-  grunt.registerTask('drone', ['execute:drone']);
-  grunt.registerTask('server', ['execute:server']);
-  grunt.registerTask('button', ['execute:button']);
+  
 
 };
